af191ddeaace0bd22a536bd963858f067b13a5eb
Daniel Bezerra <dbezerra@desenbahia.ba.gov.br>
2018-12-04 14:48:45 -0300

    Small adjustments

--- APIUsuario/Models/PagingModel.cs
+++ APIUsuario/Models/PagingModel.cs
@@ -1,22 +1,21 @@
-
-    namespace APIUsuario.Models  
+namespace APIUsuario.Models
+{  
+    public class PagingModel
     {  
-        public class PagingModel
-        {  
-            const int maxPageSize = 50;  
-      
-            public int pageNumber { get; set; } = 1;  
-      
-            public int _pageSize { get; set; } = 20;  
-      
-            public int pageSize  
-            {  
-      
-                get { return _pageSize; }  
-                set  
-                {  
-                    _pageSize = (value > maxPageSize) ? maxPageSize : value;  
-                }  
-            }  
-        }  
+        const int maxPageSize = 50;
+    
+        public int pageNumber { get; set; } = 1;
+    
+        public int _pageSize { get; set; } = 20;
+    
+        public int pageSize
+        {
+    
+            get { return _pageSize; }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
     }  
+}  
